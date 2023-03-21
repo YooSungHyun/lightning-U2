4d14a4f6ba7c7ba4e70b74a05e4e4f89adfb7cbe
bart <shyu0522@hanmail.net>
2023-03-21 18:10:31 +0900

    Update: shellscripts final

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-GPU_IDS=3
+MODEL_DIR=""
 
-CUDA_VISIBLE_DEVICES=$GPU_IDS \
 python3 inference.py \
-    --model_path="model_outputs/lightning-template-epoch=02-val_loss=0.0842-v2.ckpt" \
-    --config_path="config/dense_model.json" \
-    --seed=42 \
-    --accelerator=gpu \
-    --devices=1 \
-    --auto_select_gpus=true \
-    --model_select=rnn \
-    --truncated_bptt_steps=2
+    --model_path="${MODEL_DIR}" \
+    --model_config="${MODEL_DIR}/conformer_u2++.yaml" \
+    --vocab_path="${MODEL_DIR}/vocab.json" \
+    --beam_size=2 \
+    --decoding_chunk_size=16 \
+    --num_decoding_left_chunks=1 \
+    --simulate_streaming=true \
+    --reverse_weight=0.5 \
+    --accelerator=cpu \
+    --encoder_type=conformer \
+    --mode=attention_rescoring
