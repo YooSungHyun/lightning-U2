97f3c6f3bb0f3017eeae769e3764b92a3e2a3d30
bart <shyu0522@hanmail.net>
2023-03-21 17:31:03 +0900

    Update: sh final

--- scripts/run_train_gpu_ddp.sh
+++ scripts/run_train_gpu_ddp.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
-GPU_IDS=""
-HF_DATA_DIRS=""
-PL_DATA_DIR=""
+GPU_IDS="0,1,2,3"
+PL_DATA_DIR="
 
-OMP_NUM_THREADS=8 \
+"
+
+OMP_NUM_THREADS=16 \
 CUDA_VISIBLE_DEVICES=$GPU_IDS \
-torchrun --standalone --nnodes=1 --nproc_per_node=2 train.py \
-    --hf_data_dirs=$HF_DATA_DIRS \
-    --pl_data_dir=$PL_DATA_DIR \
+python3 -m torch.distributed.launch --master_port=55555 --nproc_per_node=4 train.py \
+    --pl_data_dir $PL_DATA_DIR \
+    --cache_main_dir= \
     --num_shards=20 \
-    --model_config="./config/config.json" \
+    --model_config="./config/conformer_u2++.yaml" \
     --vocab_path="./config/vocab.json" \
     --output_dir="../model_outputs" \
     --seed=42 \
     --num_proc=12 \
-    --per_device_train_batch_size=32 \
+    --per_device_train_batch_size=16 \
     --train_batch_drop_last=false \
-    --per_device_eval_batch_size=32 \
     --eval_batch_drop_last=false \
     --val_check_interval=0.25 \
     --accumulate_grad_batches=1 \
-    --max_epochs=40 \
-    --log_every_n_steps=400 \
+    --max_epochs=120 \
+    --log_every_n_steps=3200 \
     --accelerator=gpu \
-    --devices=1 \
+    --strategy=ddp \
+    --devices=4 \
     --learning_rate=0.001 \
-    --precision=16 \
+    --precision=32 \
     --weight_decay=0.01 \
     --warmup_ratio=0.01 \
     --final_div_factor=10 \
     --div_factor=20 \
-    --label_name=grapheme_input_ids
+    --label_name=syllabel_labels \
+    --group_by_length=true \
+    --input_name=input_values \
+    --length_column_name=length \
+    --encoder_type=conformer \
+    --detect_anomaly=true
